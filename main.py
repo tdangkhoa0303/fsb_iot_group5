29a66fbd5f9b9fece347a5107b1f147fcdf8249b
Khoa Tran <tdkhoa0303@gmail.com>
2024-03-01 14:29:24 +0700

    Send/receive data from IOT devices

--- main.py
+++ main.py
@@ -5,38 +5,46 @@ from datetime import datetime
 from Adafruit_IO import MQTTClient
 from uart import *
 
-AIO_FEED_IDS = ['button1', 'button2']
-AIO_USERNAME = 'khoadtran'
-AIO_KEY = 'aio_KlbP582DxPSRYqpGLFTLN3zf25Ow'
+AIO_FEED_IDS = ["button1", "button2", "button3"]
+AIO_USERNAME = ""
+AIO_KEY = ""
+
 
 def connected(client):
-  print("Connected...")
-  for id in AIO_FEED_IDS:
-    client.subscribe(id)
+    print("Connected...")
+    for id in AIO_FEED_IDS:
+        client.subscribe(id)
 
-def subscribe(client , userdata , mid , granted_qos):
-  print("Subscribed...")
+
+def subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed...")
+
 
 def disconnected(client):
-  print("Disconnected...")
-  sys.exit (1)
+    print("Disconnected...")
+    sys.exit(1)
 
-def message(client , feed_id , payload):
-  print(f"Receive data: {feed_id} - {payload}")
-  if feed_id == "button1":
-    if payload == "0":
-      write_data("1")
-    else:
-      write_data("0")
-  if feed_id == "button2":
-    if payload == "0":
-      write_data("3")
-    else:
-      write_data("4")
-    
-    
 
-client = MQTTClient(username=AIO_USERNAME , key=AIO_KEY)
+def message(client, feed_id, payload):
+    print(f"Receive data: {feed_id} - {payload}")
+    if feed_id == "button1":
+        if payload == "0":
+            write_data("0")
+        else:
+            write_data("1")
+    if feed_id == "button2":
+        if payload == "0":
+            write_data("3")
+        else:
+            write_data("2")
+    if feed_id == "button3":
+        if payload == "0":
+            write_data("5")
+        else:
+            write_data("4")
+
+
+client = MQTTClient(username=AIO_USERNAME, key=AIO_KEY)
 client.on_connect = connected
 client.on_disconnect = disconnected
 client.on_message = message
@@ -47,31 +55,32 @@ client.loop_background()
 counter = 5
 sensor_counter = -1
 
+
 def send_sensor_data(sensor_key, data):
-  now = datetime.now()
-  current_time = now.strftime("%H:%M:%S")
-  print(f'Sending data to {sensor_key} at {current_time}: {data}')
-  client.publish(sensor_key, data)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print(f"Sending data to {sensor_key} at {current_time}: {data}")
+    client.publish(sensor_key, data)
+
 
 # CAMERA can be 0 or 1 based on default camera of your computer
-camera = cv2.VideoCapture(0)
+# camera = cv2.VideoCapture(0)
 
 while True:
-  if counter <= 0:
-    counter = 5
-    sensor_counter += 1
-    sensor_type = sensor_counter % 3 + 1  
-    _, image = camera.read()
-    mask_result = mask_detector(image)
-    send_sensor_data('ai', mask_result)
-  
-  read_serial(client) 
-  
-  counter -= 1
-  time.sleep(1)
-  pass
+    if counter <= 0:
+        counter = 5
+        sensor_counter += 1
+        sensor_type = sensor_counter % 3 + 1
+        # _, image = camera.read()
+        # mask_result = mask_detector(image)
+        # send_sensor_data('ai', mask_result)
+
+    read_serial(client)
+
+    counter -= 1
+    time.sleep(1)
+    pass
 
 
-
-camera.release()
-cv2.destroyAllWindows()
+# camera.release()
+# cv2.destroyAllWindows()
--- uart.py
+++ uart.py
@@ -1,43 +1,56 @@
 import serial.tools.list_ports
 
+
 def get_port():
     ports = serial.tools.list_ports.comports()
     N = len(ports)
     commPort = "None"
+    print(N)
     for i in range(0, N):
         port = ports[i]
         strPort = str(port)
-        if "USB Serial Device" in strPort:
+
+        print(strPort)
+        if "Espressif Device" in strPort:
             splitPort = strPort.split(" ")
-            commPort = (splitPort[0])
+            commPort = splitPort[0]
     return commPort
-  
+
+
 if get_port() != "None":
-    ser = serial.Serial( port=get_port(), baudrate=115200)
+    ser = serial.Serial(port=get_port(), baudrate=115200)
+
 
 def process_data(client, data):
     data = data.replace("!", "")
     data = data.replace("#", "")
     splitData = data.split(":")
     print(splitData)
-    if splitData[1] == "TEMP":
-      client.publish("bbc-temp", splitData[2])
+    if splitData[1] == "T":
+        client.publish("sensor3", splitData[2])
+    elif splitData[1] == "H":
+        client.publish("sensor1", splitData[2])
+    elif splitData[1] == "L":
+        client.publish("sensor2", splitData[2])
+
 
 mess = ""
+
+
 def read_serial(client):
     bytesToRead = ser.inWaiting()
-    if (bytesToRead > 0):
+    if bytesToRead > 0:
         global mess
         mess = mess + ser.read(bytesToRead).decode("UTF-8")
         while ("#" in mess) and ("!" in mess):
             start = mess.find("!")
             end = mess.find("#")
-            process_data(client, mess[start:end + 1])
-            if (end == len(mess)):
+            process_data(client, mess[start : end + 1])
+            if end == len(mess):
                 mess = ""
             else:
-                mess = mess[end+1:]
+                mess = mess[end + 1 :]
 
 
 def write_data(data):
-  ser.write(str(data).encode())
+    ser.write(str(data).encode())
